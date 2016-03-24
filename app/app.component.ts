1b538861d86151ba81dc1a52d2c8e69d739865f0
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-03-24 08:44:22 +0200

    Initial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,39 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  templateUrl: 'app/templates/example.html',
+  directives: []
 })
-export class AppComponent { }
+export class AppComponent {
+  comboBoxLabel: string;
+  items: Array<string>;
+  constructor() {
+    var self = this;
+    this.items = ['2015-02-01', '2012-07-05'];
+    this.comboBoxLabel = 'Best dates';
+    this.eventListeners();
+  }
+
+  eventListeners() {
+    var self = this;
+    setTimeout(function() {
+      var datepicker = document.querySelector('vaadin-date-picker');
+      datepicker.addEventListener('value-changed',function() {
+        self.items.push(datepicker.value);
+        //console.log(datepicker.value);
+      });
+    },1000);
+  }
+
+  getJSON(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        callback(JSON.parse(xhr.responseText));
+      }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+  }
+}
